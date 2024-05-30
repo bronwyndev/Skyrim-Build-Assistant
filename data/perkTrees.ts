bf0aa5d836ab147044fee745cda793f94d91194d
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-05-31 09:21:15 +1000

    Add initial test data

---
+++ data/perkTrees.ts
@@ -0,0 +1,67 @@
+import { PerkTree } from '../models/perk';
+
+export const perkTrees: PerkTree[] = [
+  {
+    id: 'alch',
+    name: 'Alteration',
+    domain: 'Magic',
+    perks: [
+      {
+        id: 'perk1',
+        name: 'Novice Alteration',
+        description: 'Cast Novice level Alteration spells for half magicka.',
+        levels: 1,
+        coords: { x: 0, y: 0 },
+      },
+      {
+        id: 'perk2',
+        name: 'Apprentice Alteration',
+        description: 'Cast Apprentice level Alteration spells for half magicka.',
+        levels: 1,
+        coords: { x: 0, y: 0 },
+      },
+    ],
+  },
+  {
+    id: 'conj',
+    name: 'Conjuration',
+    domain: 'Magic',
+    perks: [
+      {
+        id: 'perk3',
+        name: 'Novice Conjuration',
+        description: 'Reduces noise while moving by 20%',
+        levels: 1,
+        coords: { x: 0, y: 0 },
+      },
+      {
+        id: 'perk4',
+        name: 'Summoner',
+        description: 'Can summon atronachs or raise undead twice as far away.',
+        levels: 2,
+        coords: { x: 0, y: 0 },
+      },
+    ],
+  },
+  {
+    id: 'dest',
+    name: 'Destruction',
+    domain: 'Magic',
+    perks: [
+      {
+        id: 'perk3',
+        name: 'Novice Destruction',
+        description: 'Cast Novice level Destruction spells for half magicka.',
+        levels: 1,
+        coords: { x: 0, y: 0 },
+      },
+      {
+        id: 'perk4',
+        name: 'Augmented Flames',
+        description: 'Fire spells do 25% more damage.',
+        levels: 2,
+        coords: { x: 0, y: 0 },
+      },
+    ],
+  },
+];

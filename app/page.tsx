7a4f85551b1593985547f9198a3eb26dc0c4123f
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-05-31 10:34:25 +1000

    Style adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="w-full">
+      <div className="w-full h-full">
         <Carousel slides={perkTrees} options={OPTIONS} />
       </div>
     </main>
--- components/organisms/Carousel.tsx
+++ components/organisms/Carousel.tsx
@@ -27,15 +27,15 @@ const Carousel: React.FC<PropType> = (props) => {
   } = usePrevNextButtons(emblaApi)
 
   return (
-    <section className="embla">
+    <section className="embla h-full">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {slides.map((tree, index) => (
             <div className="embla__slide" key={index}>
-              <div className="embla__slide__number">{tree.name}</div>
-                <div className="embla__slide__content">
-                  <PerkTreeList perks={tree.perks} />
-                </div>
+              <div className="embla__slide__content">
+                <PerkTreeList perks={tree.perks} />
+              </div>
+              <div className="embla__slide__title">{tree.name}</div>
             </div>
           ))}
         </div>

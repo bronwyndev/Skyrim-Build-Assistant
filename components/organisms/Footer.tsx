22031ea664ac629159702ebd6555eb289398d6a9
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-07-02 17:39:20 +1000

    Add footer organism

---
+++ components/organisms/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Block from '../molecules/Block';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <Block heading="Skyrim Perk Calculator" />
+            <Block heading="Socials" />
+        </footer>
+    );
+};
+
+export default Footer;

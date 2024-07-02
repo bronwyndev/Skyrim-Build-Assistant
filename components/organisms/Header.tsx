cc34faefbf29937c295d956f41d3038394cf7ced
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-07-02 18:02:31 +1000

    Add header styling

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -8,7 +8,7 @@ type PropType = {
 const Header: React.FC<PropType> = ({ totalCount }) => {
     return (
         <header className="w-full flex justify-center absolute">
-            <div className="header-block flex w-2/3 my-6 p-2 bg-black border-y border-white">
+            <div className="header-block flex w-2/3 my-6 p-2 bg-black border-y border-white relative">
                 <Block heading="Total perk count" content={totalCount} />
                 <Block heading="Level" content={totalCount + 1} />
                 <Block heading="Class" />

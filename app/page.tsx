385eeddcefc93a2c7a75fc524b96300f71909306
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-07-02 17:38:53 +1000

    Add header component to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from "next/image";
+import Header from "../components/organisms/Header";
 import Carousel from "../components/organisms/Carousel";
 import { EmblaOptionsType } from 'embla-carousel'
 import { PerkTree } from "@/models/perk";
@@ -9,6 +10,7 @@ import { useState, useEffect } from "react";
 const OPTIONS: EmblaOptionsType = { loop: true }
 
 export default function Home() {
+  const [totalCount, setTotalCount] = useState(0);
   const [perkTrees, setPerkTrees] = useState<PerkTree[]>([]);
 
   useEffect(() => {
@@ -21,6 +23,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-t-12 p-b-12">
       <div className="w-full flex flex-1">
         <Carousel slides={perkTrees} options={OPTIONS} />
+        <Header totalCount={totalCount} />
+        <Carousel slides={perkTrees} options={OPTIONS} setTotalCount={setTotalCount} />
       </div>
     </main>
   );

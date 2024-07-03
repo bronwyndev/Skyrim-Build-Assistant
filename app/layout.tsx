47207f7ff55393e1580dd4aca84d18ed28e67ac6
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-07-03 16:05:29 +1000

    Add starry background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
+import './stars.css'
 import './embla.css'
 
 const josefin = Josefin_Sans({ subsets: ["latin"] });
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from "next/image";
 import Header from "../components/organisms/Header";
 import Footer from "../components/organisms/Footer";
 import Carousel from "../components/organisms/Carousel";
@@ -15,6 +14,48 @@ export default function Home() {
   const [perkTrees, setPerkTrees] = useState<PerkTree[]>([]);
 
   useEffect(() => {
+    const starContainer = document.getElementById('star-container');
+    const numStars = 100;
+
+    for (let i = 0; i < numStars; i++) {
+      const star = document.createElement('div');
+      star.classList.add('star');
+      star.style.left = `${Math.random() * 100}%`;
+      star.style.top = `${Math.random() * 100}%`;
+      starContainer?.appendChild(star);
+
+      const circleSize = Math.random();
+      if (circleSize > 0.9) {
+        star.classList.add('large');
+      } else if (circleSize < 0.5) {
+        star.classList.add('small');
+      }
+
+      setTimeout(() => {
+        animateStar(star);
+      }, Math.random() * 5000);
+    }
+
+    function animateStar(star: HTMLDivElement) {
+      const fadeDuration = getRandomNumber(5, 10) * 1000;
+      star.style.transition = `opacity ${fadeDuration}ms`;
+      star.style.opacity = '0.6';
+
+      setTimeout(() => {
+        star.style.opacity = '0';
+      }, fadeDuration);
+
+      setTimeout(() => {
+        animateStar(star);
+      }, fadeDuration * 2 + getRandomNumber(5, 20) * 1000);
+    }
+
+    function getRandomNumber(min: number, max: number) {
+      return Math.random() * (max - min) + min;
+    }
+  }, []);
+
+  useEffect(() => {
     fetch('/api/perkTrees')
       .then((response) => response.json())
       .then((data) => setPerkTrees(data));
@@ -22,7 +63,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-t-12 p-b-12">
-      <div className="starry-sky"></div>
+      <div className="starry-sky" id="star-container"></div>
       <div className="w-full flex flex-1">
         <Header totalCount={totalCount} />
         <Carousel slides={perkTrees} options={OPTIONS} setTotalCount={setTotalCount} />

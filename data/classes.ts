b12d5c5a4e691bb404d94621a4d4e421baaa0fb8
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-07-08 22:50:24 +1000

    Add initial class data

---
+++ data/classes.ts
@@ -0,0 +1,10 @@
+import { Class } from '../models/class';
+
+export const classes: Class[] = [
+  {
+    id: 'necro',
+    name: 'Necromancer',
+    domain: 'Magic',
+    perks: ['000f2ca9']
+  }
+];

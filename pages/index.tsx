964616dfeb2cff10e49ab610e01a7611eaabc627
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-07-25 14:15:20 +1000

    Fix build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+import React from 'react';
 import LoadScreen from "../components/organisms/LoadScreen";
 import Header from "../components/organisms/Header";
 import Footer from "../components/organisms/Footer";
